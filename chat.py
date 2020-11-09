55cc5bbc33964a269c89aec0f0066d1e054901e8
Kenneth Eversole <kenneth.eversole@gmail.com>
2020-11-08 21:10:59 -0600

    added some fun additions

--- chat.py
+++ chat.py
@@ -7,7 +7,7 @@ from nltk_utils import bag_of_words, tokenize
 device = torch.device('cpu')
 
 
-with open('intents.json', 'r') as f:
+with open('intents.json', 'r+') as f:
     intents = json.load(f)
 
 FILE = "data.pth"
@@ -26,14 +26,14 @@ model.eval()
 
 
 
-bot_name = "KENBOT"
+bot_name = "bot"
 print("Let's chat! Type 'quit' to exit")
 
 while True:
-    sentence = input("You: ")
-    if sentence == "quit":
+    sentence_org = input("You: ")
+    if sentence_org == "quit":
         break
-    sentence = tokenize(sentence)
+    sentence = tokenize(sentence_org)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X)
@@ -51,4 +51,15 @@ while True:
             if tag == intent["tag"]:
                 print(f"{bot_name}: {random.choice(intent['responses'])}")
     else:
-        print(f"{bot_name}: I do not understand...")
+        print(f"{bot_name}: I do not understand... could you help me? (yes/no)")
+        response = input("You: ")
+        if response == "yes":
+            print(f"What is the subject of the message? example [{tags}]")
+            response = input("You: ")
+            response.lower()
+            if response in tags:
+                for intent in intents["intents"]:
+                    if response == intent["tag"]:
+                        intent["patterns"].append(sentence_org)
+                        print(f"{bot_name}: Adding this to my brain, thank you")
+f.close()
